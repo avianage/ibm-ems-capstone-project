49ad7400162470396e85d4539aa5101dc1fa1fa9
Rushikesh Malokar <rushikesh.malokar@ibm.com>
2026-07-10 14:17:04 +0530

    Protected api

---
+++ attendance-service/src/main/java/com/ibm/attendance/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.ibm.attendance.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+
+@OpenAPIDefinition(
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
